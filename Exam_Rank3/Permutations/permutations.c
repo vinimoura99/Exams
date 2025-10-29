720c1506e13869279af8cf6455f3099bba7e1187
Vinicius Moura <vinicius_moura98@yahoo.com>
2025-10-29 10:56:59 +0000

    Done changes on Powerset

--- Exam_Rank3/Permutations/permutations.c
+++ Exam_Rank3/Permutations/permutations.c
@@ -6,20 +6,12 @@
 /*   By: vmoura-d <vmoura-d@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 13:05:24 by vmoura-d          #+#    #+#             */
-/*   Updated: 2025/10/27 11:03:14 by vmoura-d         ###   ########.fr       */
+/*   Updated: 2025/10/29 10:46:22 by vmoura-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
-
-int ft_strlen(char *str)
-{
-    int size = 0;
-    for(int i = 0; str[i]; i++)
-        size++;
-    return(size);
-}
 
 void swap(char *a, char *b)
 {
@@ -28,54 +20,57 @@ void swap(char *a, char *b)
     *b = tmp;
 }
 
-void bubble_sort(char *str)
+int ft_strlen(char *str)
+{
+    int size = 0;
+    for(int i = 0;str[i];i++)
+        size++;
+    return(size);
+}
+
+void sort(char *str)
 {
     int len = ft_strlen(str);
-
-    for(int i = 0; i < len; i++)
+    for(int i = 0;i < len;i++)
     {
-        for(int j = i + 1; j < len; j++)
+        for(int j = i + 1;j < len;j++)
         {
             if(str[i] > str[j])
                 swap(&str[i],&str[j]);
-        }
+        }   
     }
 }
 
-void permutation(char *str, int start , int end)
+void permutation(char *str, int pos, int end_pos)
 {
-    if(start == end)
+    if(pos == end_pos)
     {
-        write(1, str, end + 1);
+        write(1, str, end_pos + 1);
         write(1, "\n", 1);
         return;
     }
 
-    int i = start;
+    int i = pos;
 
     while(str[i])
     {
-        if(i != start && str[start] == str[i])
-            continue;
-        
-        swap(&str[start],&str[i]);
-        bubble_sort(str + start + 1);
-        permutation(str, start + 1, end);
-        bubble_sort(str + start);
+        swap(&str[pos],&str[i]);
+        sort(str + pos + 1);
+        permutation(str, pos + 1, end_pos);
+        sort(str + pos);
         i++;
     }
 }
-int main(int ac , char **av)
+int main(int ac, char **av)
 {
-    //abc teu argumento ( ./permutation "abc")
     if(ac == 2)
     {
         char *str = av[1];
-        int len = ft_strlen(av[1]);
+        int len = ft_strlen(str);
 
-        bubble_sort(str);
+        sort(str);
         permutation(str,0,len - 1);
     }
-    else
+    else 
         return(1);
 }
--- Exam_Rank3/Powerset/powerset.c
+++ Exam_Rank3/Powerset/powerset.c
@@ -6,68 +6,77 @@
 /*   By: vmoura-d <vmoura-d@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 17:49:44 by vmoura-d          #+#    #+#             */
-/*   Updated: 2025/10/27 18:25:48 by vmoura-d         ###   ########.fr       */
+/*   Updated: 2025/10/29 10:56:31 by vmoura-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void	print_subset(int *arr, int *path, int len)
+void print_list(int *array, int len)
 {
-	int first = 1;
+    int i = 0;
 
-	for(int i = 0;i < len;i++)
-	{
-		if (path[i])
-		{
-			if (!first)
-				printf(" ");
-			printf("%d", arr[i]);
-			first = 0;
-		}
-	}
-	printf("\n");
-}
-void powerset(int *array, int *path, int len, int idx, int sum, int target)
-{
-    if(idx == len)
+    while (i < len)
     {
-        if(sum == target)
-        {
-            print_subset(array,path,len);
-        }
-        return;
+        if (i > 0)
+            printf(" ");
+        printf("%i", array[i]);
+        i++;
     }
-
-    path[idx] = 1;
-    powerset(array,path,len,idx + 1,sum + array[idx],target);
-    path[idx] = 0;
-    powerset(array,path,len,idx + 1,sum,target);
+    printf("\n");
 }
-int main(int ac, char **av)
+
+void powerset(int *array, int len, int target, int index, int sum, int *path, int path_size)
 {
-    if(ac < 3)
-        return(1);
+    if (index == len)
+    {
+        if (sum == target)
+            print_list(path, path_size);
+        return ;
+    }
+    powerset(array, len, target, index + 1, sum, path, path_size);
+    path[path_size] = array[index];
+    powerset(array, len, target, index + 1, sum + array[index], path, path_size + 1);
+}
+
+int main(int argc, char **argv)
+{
+    int target; //alvo a ser atingido com a soma
+    target = atoi(argv[1]);
     
-    int target = atoi(av[1]);
-    int len = ac - 2;
+    int len;
+    len = argc - 2;
+    
+    int *array;
+    array = malloc(len * sizeof(int));
+    
+    int *path;
+    path = malloc(len * sizeof(int));
 
-    int *array = malloc(sizeof(int) * len);
-    int *path = calloc(len,sizeof(int));
+    int i = 0;
 
+    if(argc < 2)
+        return(1);   
+
+    
+    
+    if (len == 0)
+        return(0); 
+
+    
+    
     if(!array || !path)
         return(1);
-    
-    for(int i = 0;i < len;i++)
+        
+    while (i < len) //popular array
     {
-        array[i] = atoi(av[i + 2]);
+        array[i] = atoi(argv[i + 2]);
+        i++;
     }
 
-    powerset(array,path,len,0,0,target);
-
+    powerset(array, len, target, 0, 0, path, 0);
+    
     free(array);
     free(path);
-    return(0);
-    
 }
